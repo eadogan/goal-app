c456ee0adff4a63c22ca7a4bcdfb324391689d3a
eadogan <e.ahmetdogan@gmail.com>
2021-05-23 15:38:13 +0100

    Create deleteHendler

--- App.js
+++ App.js
@@ -11,13 +11,25 @@ export default function App() {
     setCourseGoals(currentGoals => [...courseGoals, {id: Math.random().toString(), value: inputData}]);
   };
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler}/>
+      <GoalInput 
+        onAddGoal={addGoalHandler}
+      />
       <FlatList 
         keyExtractor={(item, index) => item.id} 
         data={courseGoals} 
-        renderItem={itemData => <GoalItem title={itemData.item.value}/>} 
+        renderItem={itemData => <GoalItem 
+                                  id={itemData.item.id} 
+                                  onDelete={removeGoalHandler} 
+                                  title={itemData.item.value}
+                                />} 
       />
     </View>
   );
